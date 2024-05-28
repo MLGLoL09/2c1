ea463d866d29250ea8cd3989108c9e51e91a71d5
MLGLoL09 <153075796+MLGLoL09@users.noreply.github.com>
2024-05-28 22:29:33 +0200

    test

---
+++ VektorMathe.java
@@ -0,0 +1,17 @@
+public class VektorMathe {
+    public static double inneres(double a, double b, double c, double d) {
+        return a*c+b*d;
+    }
+
+    public static int inneres(int a, int b, int c, int d) {
+        return a*c+b*d;
+    }
+
+    public static double inneres(double a, double b, double c, double d, double e, double f) {
+        return a*d+b*e+c*f;
+    }
+
+    public static int inneres(int a, int b, int c, int d, int e,int f) {
+        return a*d+b*e+c*f;
+    }
+}
